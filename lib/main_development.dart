ac140c16e97b6780f0b041cc2cca8b4826a06d2f
HaythamHany95 <haythamhany95@gmail.com>
2024-08-31 19:20:57 +0300

    Add Development & Production Flavors for Android
    - Added launch configurations for easy debugging in development and production modes.
    - Configured flavors in the build.gradle file.
    - Assigned distinct app names and application IDs for development and production.
    - Added main_development.dart and main_production.dart.
    - ScreenUtil setup for each mode to fix the release mode text's hidden bug.

---
+++ lib/main_development.dart
@@ -0,0 +1,12 @@
+import 'package:doctor/core/di/dependency_injection.dart';
+import 'package:doctor/core/routing/app_router.dart';
+import 'package:doctor/doctor_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  setupGetIt();
+  // To fix the bug of text being hidden in screen_util in the release mode
+  await ScreenUtil.ensureScreenSize();
+  runApp(DoctorApp(appRouter: AppRouter()));
+}
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,8 +2,11 @@ import 'package:doctor/core/di/dependency_injection.dart';
 import 'package:doctor/core/routing/app_router.dart';
 import 'package:doctor/doctor_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  // To fix the bug of text being hidden in flutter_screenutil in the release mode
+  await ScreenUtil.ensureScreenSize();
   runApp(DoctorApp(appRouter: AppRouter()));
 }
